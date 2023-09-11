156199f40d268b741f4df4c30458515be3509ea8
Hendrix Ebrudu <hendrixobuks@gmail.com>
2023-09-11 16:58:38 +0100

    initial

---
+++ main.py
@@ -0,0 +1,38 @@
+from fastapi import FastAPI, Query
+from datetime import datetime
+from pytz import utc
+
+app = FastAPI()
+
+
+@app.get("/")
+async def welcome():
+    return {"message": "Welcome to Hendrix's app. Type in your name and track as a parameter"}
+
+
+@app.get("/api")
+async def get_data(
+        slack_name: str = Query(..., title="Slack Name"),
+        track: str = Query(..., title="Track"),
+):
+    current_time = datetime.now(utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    current_day = datetime.now(utc).strftime("%A")
+
+    github_repo_url = ""
+    github_file_url = ""
+
+    response = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": current_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200,
+    }
+    return response
+
+# if __name__ == "__main__":
+#     import uvicorn
+#
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
